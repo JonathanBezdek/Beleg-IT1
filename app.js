edd3671f273107fb795334d81e9815e881b94c17
JonathanBezdek <bezdek.jonathan@gmail.com>
2023-01-12 20:05:45 +0100

    Voreinstellungen für input types und stylesheet

--- app.js
+++ app.js
@@ -1,5 +1,66 @@
 const deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
 
+
+
+function updateCurrentTime() {
+    var date = new Date();
+    var day = date.getDate();
+    var month = date.getMonth() + 1;
+    var year = date.getFullYear();
+    var hour = date.getHours();
+    var minute = date.getMinutes();
+    var second = date.getSeconds();
+
+    hour = hour < 10 ? "0" + hour : hour;
+    minute = minute < 10 ? "0" + minute : minute;
+    second = second < 10 ? "0" + second : second;
+
+    day = day < 10 ? "0" + day : day;
+    month = month < 10 ? "0" + month : month;
+    year = year < 10 ? "0" + year : year;
+
+    var current_time = hour + " : " + minute + " : " + second;
+
+    var displayDate;
+    var displayTime;
+
+    var tomorrow = new Date();
+
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    var tomorrowDay = tomorrow.getDate();
+    var tomorrowMonth = tomorrow.getMonth() + 1;
+    var tomorrowYear = tomorrow.getFullYear();
+
+    tomorrowDay = tomorrowDay < 10 ? "0" + tomorrowDay : tomorrowDay;
+    tomorrowMonth = tomorrowMonth < 10 ? "0" + tomorrowMonth : tomorrowMonth;
+    tomorrowYear = tomorrowYear < 10 ? "0" + tomorrowYear : tomorrowYear;
+
+    //Sonderfälle 22 und 23 Uhr     
+    switch (hour + 2) {
+        case 24:
+            displayTime = "00:00";
+            displayDate = tomorrowYear + "-" + tomorrowMonth + "-" + tomorrowDay;
+            break;
+        case 23:
+            displayTime = "01:00";
+            displayDate = tomorrowYear + "-" + tomorrowMonth + "-" + tomorrowDay;
+            break;
+        default:
+            displayTime = hour + 2 + ":00";
+            displayDate = year + "-" + month + "-" + day
+    }
+
+    document.getElementById("currentTime").innerHTML = current_time;
+    document.getElementById("endTime").value = displayTime;
+    document.getElementById("endDate").value = displayDate;
+    //document.getElementById('endDate').value = new Date().toDateInputValue();
+
+}
+
+setInterval(updateCurrentTime, 1000);
+
+updateCurrentTime()
+
 function getTimeRemaining(endtime) {
     const total = Date.parse(endtime) - Date.parse(new Date());
     const seconds = Math.floor((total / 1000) % 60);
